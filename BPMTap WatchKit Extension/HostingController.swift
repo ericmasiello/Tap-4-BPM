81ea737369e30aad3d4b53959d2f674ab45ebf60
Eric Masiello <eric.j.masiello@gmail.com>
2020-05-08 21:32:58 -0400

    Initial Commit

---
+++ BPMTap WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  BPMTap WatchKit Extension
+//
+//  Created by Eric Masiello on 5/8/20.
+//  Copyright © 2020 Eric Masiello. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
