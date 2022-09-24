1d7ee048ddb6ce22b5c232fe79153380a2ee3c37
Eric Masiello <emasiello@vistaprint.com>
2022-09-24 12:36:20 -0400

    create new fresh project

---
+++ BPMTap Watch App/BPMTapApp.swift
@@ -0,0 +1,17 @@
+//
+//  BPMTapApp.swift
+//  BPMTap Watch App
+//
+//  Created by Eric Masiello on 9/24/22.
+//
+
+import SwiftUI
+
+@main
+struct BPMTap_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
